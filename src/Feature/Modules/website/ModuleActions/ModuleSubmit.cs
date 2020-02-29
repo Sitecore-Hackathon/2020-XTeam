80d48decd1e5d24b78fe21b9f462fb1fb06111dd
Patrick Doran <patrick.doran@americaneagle.com>
2020-02-28 23:59:51 -0600

    Synced form and started building custom submit.

---
+++ src/Feature/Modules/website/ModuleActions/ModuleSubmit.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.ExperienceForms.Models;
+using Sitecore.ExperienceForms.Processing;
+using Sitecore.ExperienceForms.Processing.Actions;
+using static System.FormattableString;
+
+namespace xTeam.Feature.Modules.ModuleActions
+{
+    public class ModuleSubmit : SubmitActionBase<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModuleSubmit"/> class.
+        /// </summary>
+        /// <param name="submitActionData">The submit action data.</param>
+        public ModuleSubmit(ISubmitActionData submitActionData) : base(submitActionData)
+        {
+        }
+
+        /// <summary>
+        /// Tries to convert the specified <paramref name="value" /> to an instance of the specified target type.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <param name="target">The target object.</param>
+        /// <returns>
+        /// true if <paramref name="value" /> was converted successfully; otherwise, false.
+        /// </returns>
+        protected override bool TryParse(string value, out string target)
+        {
+            target = string.Empty;
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the action with the specified <paramref name="data" />.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <param name="formSubmitContext">The form submit context.</param>
+        /// <returns>
+        ///   <c>true</c> if the action is executed correctly; otherwise <c>false</c>
+        /// </returns>
+        protected override bool Execute(string data, FormSubmitContext formSubmitContext)
+        {
+            
+
+            return true;
+        }
+    }
+}
