07920b5c3f783a634661f191b54eaa6613b57dc5
James.Gregory <james.gregory@americaneagle.com>
2020-02-29 07:29:54 -0600

    Continued DEF provider

--- src/Foundation/Integration/website/GitHub/TextFileEndpointTroubleshooter.cs
+++ src/Foundation/Integration/website/GitHub/TextFileEndpointTroubleshooter.cs
@@ -1,4 +1,5 @@
-﻿using Sitecore.DataExchange.Models;
+﻿using Octokit;
+using Sitecore.DataExchange.Models;
 using Sitecore.DataExchange.Troubleshooters;
 using System;
 using System.Collections.Generic;
@@ -6,33 +7,84 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using xTeam.Foundation.Integration.GitHub;
 
-namespace Examples.DataExchange.Providers.FileSystem
+namespace xTeam.Foundation.Integration.GitHub
 {
-    public class TextFileEndpointTroubleshooter : BaseEndpointTroubleshooter
+    public class GitHubEndpointTroubleshooter : BaseEndpointTroubleshooter
     {
-        public TextFileEndpointTroubleshooter()
+        public GitHubEndpointTroubleshooter()
         {
         }
         protected override ITroubleshooterResult Troubleshoot(Endpoint endpoint, TroubleshooterContext context)
         {
-            var settings = endpoint.GetPlugin<TextFileSettings>();
+            var settings = endpoint.GetPlugin<GitHubSettings>();
             if (settings == null)
             {
-                return TroubleshooterResult.FailResult("The endpoint is missing the plugin TextFileSettings.");
+                return TroubleshooterResult.FailResult("The endpoint is missing the plugin GitHubSettings.");
             }
-            if (string.IsNullOrWhiteSpace(settings.Path))
+
+            try
             {
-                return TroubleshooterResult.FailResult("The endpoint does not have a file path specified on it.");
+                var apiInfo = settings.Client.GetLastApiInfo();
+
+                if (apiInfo == null)
+                {
+                    return TroubleshooterResult.SuccessResult(Task.Run(() => GetRateInfo(settings.Client)).Result);
+                }
+                else {
+
+                    return TroubleshooterResult.SuccessResult(GetLastRateInfo(apiInfo.RateLimit));
+                }
+
             }
-            if (!File.Exists(settings.Path))
-            {
-                return TroubleshooterResult.FailResult(
-                    "The path specified on the endpoint either " + 
-                    "points to a file that does not exist " +
-                    "or the process does not have permission to read the file.");
+            catch (Exception ex) {
+                return TroubleshooterResult.FailResult("API Error", ex);
             }
-            return TroubleshooterResult.SuccessResult("The specified file exists and can be accessed.");
+
+        }
+
+        protected async Task<string> GetRateInfo(GitHubClient client)
+        {
+            var sb = new StringBuilder();
+
+            var rateInfo = await client.Miscellaneous.GetRateLimits();
+            var coreRateLimit = rateInfo.Resources.Core;
+            sb.AppendLine("Connection Successful!").AppendLine();
+
+            var howManyCoreRequestsCanIMakePerHour = coreRateLimit.Limit;
+            sb.AppendLine($"Allowed {howManyCoreRequestsCanIMakePerHour} core requests per hour");
+
+            var howManyCoreRequestsDoIHaveLeft = coreRateLimit.Remaining;
+            var whenDoesTheCoreLimitReset = coreRateLimit.Reset; // UTC time
+            sb.AppendLine($"{howManyCoreRequestsDoIHaveLeft} core requests left until {whenDoesTheCoreLimitReset.ToLocalTime()}");
+
+            // the "search" object provides your rate limit status for the Search API.
+            var searchRateLimit = rateInfo.Resources.Search;
+            sb.AppendLine();
+            var howManySearchRequestsCanIMakePerMinute = searchRateLimit.Limit;
+            sb.AppendLine($"Allowed {howManySearchRequestsCanIMakePerMinute} search requests per minute");
+            var howManySearchRequestsDoIHaveLeft = searchRateLimit.Remaining;
+            var whenDoesTheSearchLimitReset = searchRateLimit.Reset; // UTC time
+            sb.AppendLine($"{howManySearchRequestsDoIHaveLeft} core requests left until {whenDoesTheSearchLimitReset.ToLocalTime()}");
+
+            return sb.ToString();
+        }
+
+        protected string GetLastRateInfo(RateLimit lastInfo)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("Connection Successful!").AppendLine();
+
+            var howManyCoreRequestsCanIMakePerHour = lastInfo.Limit;
+            sb.AppendLine($"Allowed {howManyCoreRequestsCanIMakePerHour} core requests per hour");
+
+            var howManyCoreRequestsDoIHaveLeft = lastInfo.Remaining;
+            var whenDoesTheCoreLimitReset = lastInfo.Reset; // UTC time
+            sb.AppendLine($"{howManyCoreRequestsDoIHaveLeft} core requests left until {whenDoesTheCoreLimitReset.ToLocalTime()}");
+
+            return sb.ToString();
         }
     }
 }
